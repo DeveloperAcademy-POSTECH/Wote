4fa3ae6a636935d776474678b9fd2953a1580a40
Kwansik Nam <kwansiknam@gmail.com>
2023-10-15 18:36:35 +0900

    feat(project): create new project

---
+++ TwoHoSun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoHoSun
+//
+//  Created by 관식 on 10/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoHoSun/TwoHoSunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoHoSunApp.swift
+//  TwoHoSun
+//
+//  Created by 관식 on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoHoSunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
