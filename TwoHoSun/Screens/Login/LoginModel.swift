dd5caf87ac0ee7f2a1e51535acbef90fc6999b90
Sunho <sunh803@naver.com>
2023-10-16 02:53:07 +0900

    feat(login): post authorizationToken and get accessToken

---
+++ TwoHoSun/Screens/Login/LoginModel.swift
@@ -0,0 +1,17 @@
+//
+//  LoginModel.swift
+//  TwoHoSun
+//
+//  Created by 235 on 10/16/23.
+//
+
+import Foundation
+struct LoginModel: Codable {
+    var status: String
+    var data: Tokens
+}
+
+struct Tokens: Codable {
+    var accessToken: String
+    var refreshToken: String
+}
