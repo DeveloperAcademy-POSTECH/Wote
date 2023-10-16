15eabb3ad33e4133f00635025a41c618a04ca84d
Sunho <sunh803@naver.com>
2023-10-16 23:48:52 +0900

    refactor(onboarding): change struct naming

--- TwoHoSun/TwoHoSunApp.swift
+++ TwoHoSun/TwoHoSunApp.swift
@@ -14,11 +14,11 @@ struct TwoHoSunApp: App {
     
     var body: some Scene {
         WindowGroup {
-            if appState.hasValidToken {
-                HomeView()
-            } else {
-                LoginView()
-            }
+//            if appState.hasValidToken {
+//                HomeView()
+//            } else {
+                OnBoardingView()
+//            }
         }
     }
 }
