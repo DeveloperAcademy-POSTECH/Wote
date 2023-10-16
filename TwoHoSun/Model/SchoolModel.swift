76856111f8e4ed8e03c410e72b6ce7f0765075e5
Mini <minnnidev@gmail.com>
2023-10-17 06:03:05 +0900

    feat(schoolSearch): add School Model

---
+++ TwoHoSun/Model/SchoolModel.swift
@@ -0,0 +1,36 @@
+//
+//  SchoolModel.swift
+//  TwoHoSun
+//
+//  Created by 김민 on 10/17/23.
+//
+
+import Foundation
+
+struct SchoolModel {
+    let schoolName: String
+    let schoolRegion: String
+    let schoolType: String
+}
+
+enum SchoolType {
+    case highSchool, middleSchool
+
+    var schoolParam: String {
+        switch self {
+        case .highSchool:
+            return "high_list"
+        case .middleSchool:
+            return "midd_list"
+        }
+    }
+
+    var schoolType: String {
+        switch self {
+        case .highSchool:
+            return "고등학교"
+        case .middleSchool:
+            return "중학교"
+        }
+    }
+}
