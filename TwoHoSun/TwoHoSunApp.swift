c33ec727c04d2f8ebd2ad15b94ecaf7c1c89bfb5
Mini <minnnidev@gmail.com>
2023-10-16 17:02:41 +0900

    Revert "fix(git): resolve conflict"
    This reverts commit d08d7ef127ec4f9d72fe60555220bb8e915de825, reversing
    changes made to f1dfb0ea1ebbbff7a4d00ce461681f0f449854d3.

--- TwoHoSun/TwoHoSunApp.swift
+++ TwoHoSun/TwoHoSunApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TwoHoSunApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+//            ContentView()
+            ProfileSettingsView()
         }
     }
 }
