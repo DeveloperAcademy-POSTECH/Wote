9d97e1a11de47d8093390d92d99dc127c7a99b65
Mini <minnnidev@gmail.com>
2023-10-21 03:41:17 +0900

    feat(notifiction): set notification list

--- TwoHoSun/Screens/Home/MainView/NotificationView.swift
+++ TwoHoSun/Screens/Home/MainView/NotificationView.swift
@@ -13,8 +13,8 @@ struct NotificationView: View {
     var body: some View {
         ZStack {
             Color.white
+            notificationList
         }
-        .ignoresSafeArea()
         .navigationBarBackButtonHidden()
         .navigationTitle("알림")
         .navigationBarTitleDisplayMode(.inline)
@@ -23,7 +23,7 @@ struct NotificationView: View {
                 backButton
             }
         }
-        .toolbarBackground(.hidden, for: .navigationBar)
+        .toolbarBackground(.white, for: .navigationBar)
     }
 }
 
@@ -38,10 +38,83 @@ extension NotificationView {
                 .foregroundStyle(.gray)
         }
     }
+
+    private var notificationList: some View {
+        List {
+            listHeader("오늘 알림")
+                .listRowSeparator(.hidden)
+            ForEach(0..<3) { _ in
+                notificationCell
+            }
+            .listRowInsets(EdgeInsets())
+            .listRowSeparator(.hidden)
+            listHeader("이전 알림")
+                .listRowSeparator(.hidden)
+            ForEach(0..<10) { _ in
+                notificationCellWithPostIcon
+            }
+            .listRowInsets(EdgeInsets())
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        .listSectionSpacing(0)
+    }
+
+    private func listHeader(_ headerName: String) -> some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Rectangle()
+                .frame(height: 1)
+            Text(headerName)
+                .font(.system(size: 14, weight: .medium))
+                .padding(.leading, 15)
+                .padding(.bottom, 10)
+        }
+        .foregroundStyle(.gray)
+    }
+
+    private var notificationCell: some View {
+        HStack(alignment: .top, spacing: 14) {
+            Circle()
+                .frame(width: 46, height: 46)
+                .foregroundStyle(.gray)
+            VStack(alignment: .leading, spacing: 6) {
+                Text("OO님이 회원님의 게시글에 답글을 남겼어요.")
+                    .font(.system(size: 16, weight: .medium))
+                Text("2분 전")
+                    .font(.system(size: 14))
+                    .foregroundStyle(.gray)
+            }
+        }
+        .padding(.horizontal, 26)
+        .padding(.vertical, 15)
+    }
+
+    private var notificationCellWithPostIcon: some View {
+        HStack(alignment: .top, spacing: 0) {
+            Circle()
+                .frame(width: 46, height: 46)
+                .foregroundStyle(.gray)
+                .padding(.trailing, 14)
+            VStack(alignment: .leading) {
+                Text("OO님이 회원님의 게시글에 답글을\n남겼어요.")
+                    .font(.system(size: 16, weight: .medium))
+                Spacer()
+                Text("2분 전")
+                    .font(.system(size: 14))
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 40, height: 40)
+                .foregroundStyle(.gray)
+        }
+        .padding(.horizontal, 26)
+        .padding(.vertical, 15)
+    }
 }
 
 #Preview {
-    NavigationStack {
+    NavigationView {
         NotificationView()
     }
 }
