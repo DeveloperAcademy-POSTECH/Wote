71b2135aa3540004ef8cc5737ab754d92ba81da1
Kwansik Nam <kwansiknam@gmail.com>
2023-10-22 01:44:23 +0900

    feat(search): add recent search chip

--- TwoHoSun/Screens/Home/MainView/SearchView.swift
+++ TwoHoSun/Screens/Home/MainView/SearchView.swift
@@ -12,9 +12,13 @@ struct SearchView: View {
     @State private var searchText: String = ""
     
     var body: some View {
-        VStack {
-            
+        VStack(alignment: .leading, spacing: 16) {
+            Divider()
+            recentSearch
+            Spacer()
+                .frame(maxWidth: .infinity)
         }
+        .padding(.horizontal, 12)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 backButton
@@ -38,6 +42,33 @@ extension SearchView {
                 .font(.system(size: 20))
                 .foregroundStyle(.gray)
         }
+    }
+    
+    private var recentSearch: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("최근 검색어")
+                .foregroundStyle(.gray)
+                .font(.system(size: 14))
+            Button {
+                
+            } label: {
+                HStack(spacing: 5) {
+                    Text("label")
+                        .font(.system(size: 14))
+                    Image(systemName: "xmark")
+                        .font(.system(size: 12))
+                }
+                .foregroundStyle(.gray)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 7)
+                .background(
+                    Capsule()
+                        .stroke(Color.gray, lineWidth: 1)
+                        .foregroundStyle(.white)
+                )
+            }
+        }
+        .padding(.horizontal, 14)
     }
 }
 
