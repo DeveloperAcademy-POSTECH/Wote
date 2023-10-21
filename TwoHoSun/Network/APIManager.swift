d12b9465872e3afeba1c1074d8d631471752e437
Kwansik Nam <kwansiknam@gmail.com>
2023-10-21 12:27:52 +0900

    feat(apiManager): add integrated apiManager

--- TwoHoSun/Network/APIManager.swift
+++ TwoHoSun/Network/APIManager.swift
@@ -12,9 +12,106 @@ import Alamofire
 
 class APIManager {
     static let shared = APIManager()
-    private init() {}
+    private init() { }
     
     private var cancellable: Set<AnyCancellable> = []
+    
+    enum HttpMethod {
+        case get, post, put, delete
+    }
+    
+    enum APIRequest {
+        case postAuthorCode(String)
+        case postNickname
+        case postProfileSetting(ProfileSetting)
+        
+        var contentType: String {
+            switch self {
+            case .postAuthorCode:
+                return "application/x-www-form-urlencoded; charset=UTF-8"
+            case .postNickname:
+                return "application/json"
+            case .postProfileSetting:
+                return "application/json"
+            }
+        }
+        
+        var method: HTTPMethod {
+            switch self {
+            case .postAuthorCode:
+                return .post
+            case .postNickname:
+                return .post
+            case .postProfileSetting:
+                return .post
+            }
+        }
+        
+        var parameters: [String: Any] {
+            switch self {
+            case .postAuthorCode(let auth):
+                return [
+                    "state": "test",
+                    "code": auth
+                ]
+            case .postNickname:
+                return [
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer \(KeychainManager.shared.readToken(key: "accessToken")!)"
+                ]
+            case .postProfileSetting(let model):
+                return [
+                    "userProfileImage": model.userProfileImage,
+                    "userNickname": model.userNickname,
+                    "school": [
+                        "schoolName": model.school.schoolName,
+                        "schoolRegion": model.school.schoolRegion,
+                        "schoolType": model.school.schoolType
+                    ],
+                    "grade": model.grade
+                ]
+            }
+        }
+        
+        var url: String {
+            switch self {
+            case .postAuthorCode:
+                return "/login/oauth2/code/apple"
+            case .postNickname:
+                return "/api/profiles/isValidNickname"
+            case .postProfileSetting:
+                return "/api/profiles"
+            }
+        }
+    }
+
+    func requestAPI<T: Decodable>(type: APIRequest, completion: @escaping (GeneralResponse<T>) -> Void) {
+        let headers: HTTPHeaders = [
+            "Content-Type": type.contentType
+        ]
+        let parameters = type.parameters
+        let url = URLConst.baseURL + type.url
+        
+        AF.request(
+            url,
+            method: type.method,
+            parameters: parameters,
+            encoding: URLEncoding.default,
+            headers: headers
+        )
+        .publishDecodable(type: GeneralResponse<T>.self)
+        .value()
+        .receive(on: DispatchQueue.main)
+        .sink(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                print(error)
+            }
+        }, receiveValue: completion )
+        .store(in: &cancellable)
+    }
     
     func refreshAllTokens() {
         let requestURL = URLConst.baseURL + "/api/auth/refresh"
