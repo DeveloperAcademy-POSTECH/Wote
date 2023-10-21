7ab741a77d0cd72e76f44f8365385c91ef09c051
Kwansik Nam <kwansiknam@gmail.com>
2023-10-21 12:29:16 +0900

    feat(apiManager): apply apiManager to postAuthorCode

--- TwoHoSun/Screens/OnBoarding/LoginViewModel.swift
+++ TwoHoSun/Screens/OnBoarding/LoginViewModel.swift
@@ -14,37 +14,22 @@ class LoginViewModel: ObservableObject {
     @Published var navigationPath: [Route] = []
     private var cancellables: Set<AnyCancellable> = []
 
-    func postAuthorCode(_ authorizationCode: String) {
-        let headers: HTTPHeaders = [
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-        ]
-        let parameters = [
-            "state": "test",
-            "code": authorizationCode
-        ]
-        let url = "https://test.hyunwoo.tech/login/oauth2/code/apple"
-        AF.request(url, method: .post, parameters: parameters, encoding: URLEncoding.httpBody, headers: headers)
-            .publishDecodable(type: GeneralResponse<Tokens>.self)
-            .value()
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error)
-                }
-            }, receiveValue: { data in
-                if let data = data.data {
+    func postAuthorCode(_ authorization: String) {
+        APIManager.shared.requestAPI(type: .postAuthorCode(authorization)) { (response: GeneralResponse<Tokens>) in
+            if response.status == 401 {
+                APIManager.shared.refreshAllTokens()
+                self.postAuthorCode(authorization)
+            } else {
+                if let data = response.data {
                     KeychainManager.shared.saveToken(key: "accessToken", token: data.accessToken)
                     KeychainManager.shared.saveToken(key: "refreshToken", token: data.refreshToken)
                 }
-                if data.message == "UNREGISTERED_USER" {
+                if response.message == "UNREGISTERED_USER" {
                     self.showSheet = true
                 } else {
                     self.navigationPath.append(.mainTabView)
                 }
-            })
-            .store(in: &cancellables)
+            }
+        }
     }
 }
