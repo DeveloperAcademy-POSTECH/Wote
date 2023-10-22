0197099c11c4791a923cdfc1fded93e42ce62df5
Mini <minnnidev@gmail.com>
2023-10-22 22:51:53 +0900

    feat(profileSettings): change region format

--- TwoHoSun/Model/DTO/SchoolResponseModel.swift
+++ TwoHoSun/Model/DTO/SchoolResponseModel.swift
@@ -24,8 +24,9 @@ struct HighSchoolContent: Codable, Hashable {
     let seq: String
 
     func convertToSchoolInfoModel() -> SchoolInfoModel {
+        let formattedRegion = regionMapping[region] ?? region
         return SchoolInfoModel(school: SchoolModel(schoolName: schoolName,
-                                                   schoolRegion: region,
+                                                   schoolRegion: formattedRegion,
                                                    schoolType: SchoolType.highSchool.schoolType),
                                schoolAddress: adres)
     }
@@ -46,9 +47,30 @@ struct MiddleSchoolContent: Codable, Hashable {
     let seq: String
 
     func convertToSchoolInfoModel() -> SchoolInfoModel {
+        let formattedRegion = regionMapping[region] ?? region
         return SchoolInfoModel(school: SchoolModel(schoolName: schoolName,
-                                                   schoolRegion: region,
+                                                   schoolRegion: formattedRegion,
                                                    schoolType: SchoolType.middleSchool.schoolType),
                                schoolAddress: adres)
     }
 }
+
+let regionMapping: [String: String] = [
+    "서울특별시": "서울",
+    "부산광역시": "부산",
+    "인천광역시": "인천",
+    "대구광역시": "대구",
+    "광주광역시": "광주",
+    "대전광역시": "대전",
+    "울산광역시": "울산",
+    "세종특별자치시": "세종",
+    "경기도": "경기",
+    "강원특별자치도": "강원",
+    "충청남도": "충남",
+    "충청북도": "충북",
+    "경상북도": "경북",
+    "경상남도": "경남",
+    "전라북도": "전북",
+    "전라남도": "전남",
+    "제주특별자치도": "제주"
+]
