cfda2a14cb3f88a07b9d12c68687b5d3560561d7
Kwansik Nam <kwansiknam@gmail.com>
2023-10-24 08:16:35 +0900

    fix(search): add navigation back gesture

--- TwoHoSun/Screens/Home/MainView/SearchView.swift
+++ TwoHoSun/Screens/Home/MainView/SearchView.swift
@@ -203,3 +203,13 @@ private struct WrappingHStack: Layout {
         }
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
