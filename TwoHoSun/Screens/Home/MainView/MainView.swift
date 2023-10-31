57cbb84d5fda2af1fc63505ce8e5f2558d1146b5
Sunho <sunh803@naver.com>
2023-10-31 17:02:27 +0900

    feat: color extension

--- TwoHoSun/Screens/Home/MainView/MainView.swift
+++ TwoHoSun/Screens/Home/MainView/MainView.swift
@@ -39,6 +39,7 @@ struct MainView: View {
     var body: some View {
         NavigationStack {
             ZStack(alignment: .bottomTrailing) {
+                Color.backGround
                 if viewModel.loading {
                     ProgressView("Loading")
                 } else {
