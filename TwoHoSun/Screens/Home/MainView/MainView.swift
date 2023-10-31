a5638d408e84751ab63b1c52133a6ccab1db08ae
Kwansik Nam <kwansiknam@gmail.com>
2023-11-01 03:49:44 +0900

    feat(onboarding): add loginView design

--- TwoHoSun/Screens/Home/MainView/MainView.swift
+++ TwoHoSun/Screens/Home/MainView/MainView.swift
@@ -68,7 +68,7 @@ struct MainView: View {
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .principal) {
-                    Image("splash")
+                    Image("logo")
                         .resizable()
                         .frame(width: 120,height: 36)
                 }
