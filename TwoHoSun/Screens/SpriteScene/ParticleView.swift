d8c681ac5dbb801173f6bdc4434e78485959e4db
migusdn <migusdn@gmail.com>
2023-11-09 01:50:14 +0900

    feat(typeTest): add particle effect

---
+++ TwoHoSun/Screens/SpriteScene/ParticleView.swift
@@ -0,0 +1,27 @@
+//
+//  ParticleView.swift
+//  TwoHoSun
+//
+//  Created by HyunwooPark on 11/9/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct ParticleView: View {
+    var scene: SKScene {
+        let scene = CelebrateScene()
+        scene.size = CGSize(width: 300, height: 300)
+        return scene
+    }
+
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 300)
+    }
+}
+
+#Preview {
+    ParticleView()
+}
+
