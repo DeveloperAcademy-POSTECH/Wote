1aa66bb0d4855c6fd689d4e52d83a8f90cfc60db
migusdn <migusdn@gmail.com>
2023-11-09 01:50:14 +0900

    feat(typeTest): add particle effect

---
+++ TwoHoSun/Screens/SpriteScene/CelebrateScean.swift
@@ -0,0 +1,26 @@
+//
+//  CelebrateScean.swift
+//  TwoHoSun
+//
+//  Created by HyunwooPark on 11/9/23.
+//
+
+import SpriteKit
+
+class CelebrateScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor.background
+        scaleMode = .resizeFill
+        
+        // 각 이미지에 대한 파티클 노드를 생성합니다.
+        for i in 0...10 {
+            if let particleEmitter = SKEmitterNode(fileNamed: "sprite") {
+                particleEmitter.position = CGPoint(x: size.width / 2, y: size.height)
+                particleEmitter.particleTexture = SKTexture(imageNamed: "sprite\(i)")
+                particleEmitter.particleBirthRate = 1 // 각 파티클 에미터의 생성 비율을 조정합니다.
+                addChild(particleEmitter)
+            }
+        }
+    }
+}
+
