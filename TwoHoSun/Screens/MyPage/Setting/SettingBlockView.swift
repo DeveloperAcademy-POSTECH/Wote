f25ec41759e9ee46744fc32dcba6e049afe8062c
Kwansik Nam <kwansiknam@gmail.com>
2023-11-13 14:49:28 +0900

    feat(setting): add empty view on settingBlockView

--- TwoHoSun/Screens/MyPage/Setting/SettingBlockView.swift
+++ TwoHoSun/Screens/MyPage/Setting/SettingBlockView.swift
@@ -27,46 +27,19 @@ struct SettingBlockView: View {
         ZStack {
             Color.background
                 .ignoresSafeArea()
-            ScrollView {
-                Divider()
-                    .foregroundStyle(Color.dividerGray)
-                ForEach($blockList) { $user in
-                    HStack {
-                        Image("defaultProfile")
-                            .resizable()
-                            .frame(width: 32, height: 32)
-                            .padding(.trailing, 2)
-                        Text(user.userName)
-                            .foregroundStyle(.white)
-                            .font(.system(size: 16, weight: .bold))
-                        Spacer()
-                        Button {
-                            user.isBlock.toggle()
-                        } label: {
-                            ZStack {
-                                if user.isBlock {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .frame(width: 103, height: 44)
-                                        .foregroundStyle(Color.lightBlue)
-                                } else {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .strokeBorder(lineWidth: 1)
-                                        .frame(width: 103, height: 44)
-                                        .foregroundStyle(Color.lightBlue)
-                                }
-                                Text("차단중")
-                                    .font(.system(size: 14, weight: .medium))
-                                    .foregroundStyle(user.isBlock ? .white : Color.lightBlue)
-                            }
-                        }
-                    }
-                        .padding(.horizontal, 8)
+            if blockList.isEmpty {
+                Text("차단한 사용자가 없습니다.")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundStyle(Color.subGray1)
+            } else {
+                ScrollView {
                     Divider()
                         .foregroundStyle(Color.dividerGray)
+                    blockListView
                 }
+                .padding(.horizontal, 16)
+                .padding(.top, 8)
             }
-            .padding(.horizontal, 16)
-            .padding(.top, 8)
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbarBackground(Color.background)
@@ -77,6 +50,45 @@ struct SettingBlockView: View {
                     .foregroundStyle(.white)
                     .font(.system(size: 18, weight: .medium))
             }
+        }
+    }
+}
+
+extension SettingBlockView {
+    private var blockListView: some View {
+        ForEach($blockList) { $user in
+            HStack {
+                Image("defaultProfile")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .padding(.trailing, 2)
+                Text(user.userName)
+                    .foregroundStyle(.white)
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+                Button {
+                    user.isBlock.toggle()
+                } label: {
+                    ZStack {
+                        if user.isBlock {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 103, height: 44)
+                                .foregroundStyle(Color.lightBlue)
+                        } else {
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(lineWidth: 1)
+                                .frame(width: 103, height: 44)
+                                .foregroundStyle(Color.lightBlue)
+                        }
+                        Text("차단중")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundStyle(user.isBlock ? .white : Color.lightBlue)
+                    }
+                }
+            }
+            .padding(.horizontal, 8)
+            Divider()
+                .foregroundStyle(Color.dividerGray)
         }
     }
 }
