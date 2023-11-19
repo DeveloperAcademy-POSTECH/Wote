e336fc7ce63f4079851d9a96daf372f1d6008f9c
Kwansik Nam <kwansiknam@gmail.com>
2023-11-20 03:15:48 +0900

    feat(myPage): add userProfileModel

---
+++ TwoHoSun/Model/UserProfileModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileModel.swift
+//  TwoHoSun
+//
+//  Created by 관식 on 11/20/23.
+//
+
+import Foundation
+
+struct UserProfileModel {
+    var createDate: String
+    var modifiedDate: String
+    var lastSchoolRegisterDate: String
+    var nickname: String
+    var profileImage: String
+    var consumerType: ConsumerType
+    var schoolName: String
+}
