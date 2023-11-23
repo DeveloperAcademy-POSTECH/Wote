fd647f9ab8825b58ac493ca37077962762091b56
Mini <minnnidev@gmail.com>
2023-11-24 08:07:41 +0900

    feat(typeTest): hide navigation bar bottom line

--- TwoHoSun/Screens/MyPage/MyPageView.swift
+++ TwoHoSun/Screens/MyPage/MyPageView.swift
@@ -103,7 +103,6 @@ struct MyPageView: View {
         .background(Color.background)
         .onAppear {
             if !didFinishSetup {
-                viewModel.fetchProfile()
                 viewModel.fetchPosts()
                 didFinishSetup = true
             }
