88d67cf9c23568dba4640f8d916082b46a2f5d69
Kwansik Nam <kwansiknam@gmail.com>
2023-11-24 04:03:32 +0900

    feat(block): add blockUser method

--- TwoHoSun/TwoHoSunApp.swift
+++ TwoHoSun/TwoHoSunApp.swift
@@ -5,6 +5,7 @@
 //  Created by 관식 on 10/15/23.
 //
 
+import Combine
 import SwiftUI
 import Observation
 
@@ -38,12 +39,27 @@ extension TwoHoSunApp {
 }
 class ServiceRoot {
     var auth = Authenticator()
-
+    var navigationManager = NavigationManager()
+    private var cancellable = Set<AnyCancellable>()
     lazy var apimanager: NewApiManager = {
         let manager = NewApiManager(authenticator: auth)
         return manager
     }()
-    var navigationManager = NavigationManager()
+    
+    private func blockUser(memberId: Int) {
+        apimanager.request(.userService(.blockUser(memberId: memberId)), decodingType: NoData.self)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error)
+                }
+            } receiveValue: { _ in
+                print("block user successful!")
+            }
+            .store(in: &cancellable)
+    }
 }
 
 @Observable
